990cd3409e386a5896b52a5db9dcae2f4e7af4ee
Jeffrey Dowdle <jeffrey.dowdle@dpc.vic.gov.au>
2023-06-21 10:29:17 +1000

    Initial

---
+++ app.config.ts
@@ -0,0 +1,16 @@
+import { defineAppConfig } from '#imports'
+import pkg from './package.json'
+
+export default defineAppConfig({
+  project: {
+    name: pkg.name,
+    version: pkg.version,
+  },
+  ripple: {
+    version: pkg.dependencies['@dpc-sdp/nuxt-ripple'],
+    featureFlags: {
+      contentCollectionSearchConnector: 'elasticsearch'
+    },
+    theme: {}
+  }
+})
